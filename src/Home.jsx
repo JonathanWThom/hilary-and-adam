c9fe60df08a417ddec1113c23e792b2d2dcfb02e
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-06-15 10:46:36 -0700

    Adds stock image for home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./Home.css";
+
+import main from "./images/main.jpg";
 
 const Home = (props) => {
   return(
     <React.Fragment>
-      Home!
+      <img className="image" src={main} alt="Hilary and Adam" />;
     </React.Fragment>
   )
 }
