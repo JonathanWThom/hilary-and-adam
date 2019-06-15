a4a93c94be52cba0847c96e60d928abe117d330d
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-06-14 21:40:02 -0700

    One more commit

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ function App() {
         <p>
           Hilary and Adam
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
