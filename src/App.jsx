2fd1a2cbbfec10735f4d543c523d9fd49a924286
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-06-16 14:00:41 -0700

    Footer positioning

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ export default class App extends React.Component {
         <section>
           { this.getPage() }
         </section>
-        <footer>
+        <footer className="footer">
           The footer goes here
         </footer>
       </React.Fragment>
