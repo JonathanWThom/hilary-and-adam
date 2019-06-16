4a742ac66f984e31f9c20570fa3319f9d3d58e52
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-06-16 14:07:50 -0700

    Adds a couple of real photos

--- src/Images.js
+++ src/Images.js
@@ -1,8 +1,20 @@
 import main from "./images/main.jpg";
+import hilaryadam1 from "./images/hilaryadam1.jpg";
+import hilaryadam2 from "./images/hilaryadam2.jpg";
 
 export const IMAGES = [
   {
     src: main,
+    width: 4,
+    height: 3
+  },
+  {
+    src: hilaryadam1,
+    width: 4,
+    height: 3
+  },
+  {
+    src: hilaryadam2,
     width: 4,
     height: 3
   }
