0c27c891c4d60a50a7f46b01a07fa72a4e8de45f
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-06-27 10:33:36 -0700

    Started adding filler content to registry

--- src/Registry.jsx
+++ src/Registry.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import registries from "./registries.js";
 
 const Registry = (props) => {
   return(
     <React.Fragment>
-      Registry!
+      {
+        registries.map(registry => {
+          return(
+            <div key={registry.name}>
+              <h1>
+                <a href={registry.url}>
+                  {registry.name}
+                </a>
+              </h1>
+              <img alt={registry.name} src={registry.src}></img>
+            </div>
+          )
+        })
+      }
     </React.Fragment>
+
   )
 }
 
---
+++ src/registries.js
@@ -0,0 +1,23 @@
+import target from "./images/target.jpg";
+import bedbathbeyond from "./images/bedbathbeyond.jpg";
+import amazon from "./images/amazon.jpg";
+
+const registries = [
+  {
+    name: "Target",
+    url: "https://www.target.com/",
+    src: target,
+  },
+  {
+    name: "Amazon",
+    url: "https://www.amazon.com",
+    src: amazon,
+  },
+  {
+    name: "Bed Bath & Beyond",
+    url: "https://www.bedbathandbeyond.com/",
+    src: bedbathbeyond,
+  }
+]
+
+export default registries;
