4b36ebbc17a2d5af28f58c487c907868355be05c
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-07-07 14:48:24 -0700

    Mobile navigation

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -4,6 +4,7 @@ import {
   ACCOMMODATIONS,
   ACTIVITIES,
   CEREMONY,
+  HOME,
   PHOTOS,
   REGISTRY,
   RSVP
@@ -18,28 +19,42 @@ const Navigation = (props) => {
     return "nav-link";
   }
 
-  const { setPage } = props;
+  const { setPage, page } = props;
 
   return(
     <React.Fragment>
-      <h4 className={className(CEREMONY)} onClick={() => setPage(CEREMONY)}>
-        Ceremony &amp; Reception
-      </h4>
-      <h4 className={className(RSVP)} onClick={() => setPage(RSVP)}>
-        RSVP
-      </h4>
-      <h4 className={className(ACCOMMODATIONS)} onClick={() => setPage(ACCOMMODATIONS)}>
-        Accomodations
-      </h4>
-      <h4 className={className(ACTIVITIES)} onClick={() => setPage(ACTIVITIES)}>
-        Activities
-      </h4>
-      <h4 className={className(REGISTRY)} onClick={() => setPage(REGISTRY)}>
-        Registry
-      </h4>
-      <h4 className={className(PHOTOS)} onClick={() => setPage(PHOTOS)}>
-        Gallery
-      </h4>
+      <div className="nav__main">
+        <h4 className={className(CEREMONY)} onClick={() => setPage(CEREMONY)}>
+          Ceremony &amp; Reception
+        </h4>
+        <h4 className={className(RSVP)} onClick={() => setPage(RSVP)}>
+          RSVP
+        </h4>
+        <h4 className={className(ACCOMMODATIONS)} onClick={() => setPage(ACCOMMODATIONS)}>
+          Accomodations
+        </h4>
+        <h4 className={className(ACTIVITIES)} onClick={() => setPage(ACTIVITIES)}>
+          Activities
+        </h4>
+        <h4 className={className(REGISTRY)} onClick={() => setPage(REGISTRY)}>
+          Registry
+        </h4>
+        <h4 className={className(PHOTOS)} onClick={() => setPage(PHOTOS)}>
+          Gallery
+        </h4>
+      </div>
+
+      <div className="nav__mobile">
+        <select value={page} onChange={e => setPage(e.target.value)}>
+          <option value={HOME}>Home</option>
+          <option value={CEREMONY}>Ceremony &amp; Reception</option>
+          <option value={RSVP}>RSVP</option>
+          <option value={ACCOMMODATIONS}>Accomodations</option>
+          <option value={ACTIVITIES}>Activities</option>
+          <option value={REGISTRY}>Registry</option>
+          <option value={PHOTOS}>Gallery</option>
+        </select>
+      </div>
     </React.Fragment>
   )
 }
