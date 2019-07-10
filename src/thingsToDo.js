c2d7de886bb2a5a83738fd3209f90dce320c3801
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-07-09 21:36:40 -0700

    More images and favicon

--- src/thingsToDo.js
+++ src/thingsToDo.js
@@ -1,53 +1,60 @@
-import hilaryadam1 from "./images/hilaryadam1.jpg";
+import twomedicine from "./images/twomedicine.jpeg";
+import stmary from "./images/stmary.jpeg";
+import glacierparklodge from "./images/glacierparklodge.jpeg";
+import glacierraft from "./images/glacierraft.jpeg";
+import plainsindian from "./images/plainsindian.jpeg";
+import swanmountain from "./images/swanmountain.jpeg";
+import easternalpine from "./images/easternalpine.jpeg";
+import manyglacier from "./images/manyglacier.jpeg";
 
 const thingsToDo = [
   {
     name: "Hike Two Medicine",
     website: "https://www.nps.gov/glac/planyourvisit/hikingtwomedicine.htm",
     notes: "Suggested Trails: Running Eagle Falls Trail, Upper Two Medicine Trail, Two Medicine Lake Loop, or Appistoki Falls.",
-    src: hilaryadam1
+    src: twomedicine
   },
   {
     name: "Hike St. Mary",
     website: "https://www.nps.gov/glac/planyourvisit/hikingstmary.htm",
     notes: "Suggested trails: Baring Falls, St. Mary Falls, or Hidden Lake",
-    src: hilaryadam1
+    src: stmary
   },
   {
     name: "Glacier Park Lodge Golf Course",
     website: "https://www.glacierparkcollection.com/glacier-national-park/east-glacier/golf-at-glacier-park-lodge/",
     notes: "",
-    src: hilaryadam1
+    src: glacierparklodge
   },
   {
     name: "Museum of the Plains Indian",
     website: "https://www.doi.gov/iacb/museum-plains-indian	Browning",
     notes: "",
-    src: hilaryadam1
+    src: plainsindian
   },
   {
     name: "Glacier Raft Company",
     website: "https://glacierraftco.com/",
     notes: "",
-    src: hilaryadam1
+    src: glacierraft
   },
   {
     name: "Swan Mountain Glacier Rides",
     website: "https://www.glacierparkcollection.com/glacier-national-park/east-glacier/golf-at-glacier-park-lodge/",
     notes: "",
-    src: hilaryadam1
+    src: swanmountain
   },
   {
     name: "Eastern Alpine Tour",
     website: "https://www.glaciernationalparklodges.com/red-bus-tours/east-side-tours/",
     notes: "Glacier National Park Red Bus Tour",
-    src: hilaryadam1
+    src: easternalpine
   },
   {
     name: "East side Many Glacier Crown of the Continent",
     website: "https://www.glaciernationalparklodges.com/red-bus-tours/east-side-tours/",
     notes: "Glacier National Park Red Bus Tour",
-    src: hilaryadam1
+    src: manyglacier
   }
 ];
 
