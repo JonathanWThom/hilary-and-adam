26998d00075098efe9dc170c4a5232bae32b2d6d
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-07-09 21:23:25 -0700

    Adds another pic of adam and hilary

--- src/Images.js
+++ src/Images.js
@@ -1,6 +1,7 @@
 import main from "./images/main.jpg";
 import hilaryadam1 from "./images/hilaryadam1.jpg";
 import hilaryadam2 from "./images/hilaryadam2.jpg";
+import hilaryadam3 from "./images/hilaryadam3.jpg";
 
 export const IMAGES = [
   {
@@ -17,5 +18,10 @@ export const IMAGES = [
     src: hilaryadam2,
     width: 4,
     height: 3
+  },
+  {
+    src: hilaryadam3,
+    width: 3,
+    height: 4
   }
 ];
