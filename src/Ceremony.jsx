705e338cc7ed39f3af082eda0ca5f510cad39020
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-07-13 15:11:58 -0700

    New locations with maps

--- src/Ceremony.jsx
+++ src/Ceremony.jsx
@@ -4,13 +4,25 @@ import "./Ceremony.css";
 const Ceremony = (props) => {
   return(
     <div className="ceremony text-align-center">
-      <h3>July 18, 2020 at 3 pm</h3>
-      <h3>Glacier Park Lodge</h3>
+      <h3>Ceremony - July 18, 2020 at 4:30 pm</h3>
+      <h3>Lone Elk Lodge</h3>
       <section className="map__container">
         <iframe
-          title="Map to Glacier Park Lodge"
+          title="Map to Lone Elk Lodge"
           frameBorder="0"
-          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ8b6sx1ZAZlMRYtFBx5_LVOs&key=AIzaSyAZFvNwSQZiqtBmIO6CGR4AMJ5I_JEexVU"
+          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ2StxAomOaFMRZ4gj-vFx9wo&key=AIzaSyAZFvNwSQZiqtBmIO6CGR4AMJ5I_JEexVU"
+          allowFullScreen>
+        </iframe>
+      </section>
+      <br />
+      <br />
+      <h3>Reception - 6:30 pm</h3>
+      <h3>The Summit Mountain Lodge</h3>
+      <section className="map__container">
+        <iframe
+          title="Map to The Summit Mountain Lodge"
+          frameBorder="0"
+          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJPb-TCOiGaFMRUw84e-bRBFg&key=AIzaSyAZFvNwSQZiqtBmIO6CGR4AMJ5I_JEexVU"
           allowFullScreen>
         </iframe>
       </section>
