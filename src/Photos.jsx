2065988a76c781557a721d8f7801b3a9cf28f0a7
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-07-21 09:43:31 -0700

    Adds real photos to gallery

--- src/Photos.jsx
+++ src/Photos.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
-import { IMAGES } from "./Images";
+// import { IMAGES } from "./Images";
 
 const Photos = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -11,20 +11,31 @@ const Photos = () => {
     setCurrentImage(obj.index);
     setViewerIsOpen(true);
   };
+
   const closeLightbox = () => {
     setCurrentImage(0);
     setViewerIsOpen(false);
   };
 
+  const importAll = (r) => {
+    return r.keys().map(r);
+  }
+
+  const IMAGES = importAll(require.context('./images/website/', false, /\.(png|jpe?g|svg)$/));
+  const PARSED_IMAGES = IMAGES.map(image => {
+    const size = image.split(".jpg")[0].split(".")[0].slice(-2).split("");
+    return { src: image, width: parseInt(size[0]), height: parseInt(size[1]) };
+  })
+
   return (
     <div className="top-margin">
-      <Gallery photos={IMAGES} onClick={openLightbox} />
+      <Gallery photos={PARSED_IMAGES} onClick={openLightbox} />
       <ModalGateway>
         {viewerIsOpen ? (
           <Modal onClose={closeLightbox}>
             <Carousel
               currentIndex={currentImage}
-              views={IMAGES.map(x => ({
+              views={PARSED_IMAGES.map(x => ({
                 ...x,
                 srcset: x.srcSet,
                 caption: x.title
