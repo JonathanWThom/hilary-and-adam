a745e76b91eccef305a74de45971f8b6a2804ef9
Jonathan Thom <jonathan.thom1990@gmail.com>
2019-07-21 09:46:00 -0700

    Changes home image

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Home.css";
-
-import main from "./images/main.jpg";
+import main from "./images/website/Main_1--34.jpg";
 
 const Home = (props) => {
   return(
