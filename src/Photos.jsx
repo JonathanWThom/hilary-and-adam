6f3b0f98fc66064ac1b5cc55bc6edb0dfe9756c3
Jonathan Thom <jonathan.thom1990@gmail.com>
2020-03-08 16:00:59 -0700

    Gallery mode only for photos

--- src/Photos.jsx
+++ src/Photos.jsx
@@ -1,21 +1,8 @@
 import React, { useState } from "react";
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
-// import { IMAGES } from "./Images";
+import Carousel from "react-images";
 
 const Photos = () => {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-  const openLightbox = (event, obj) => {
-    setCurrentImage(obj.index);
-    setViewerIsOpen(true);
-  };
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
+  const [currentImage] = useState(0);
 
   const importAll = (r) => {
     return r.keys().map(r);
@@ -30,21 +17,14 @@ const Photos = () => {
 
   return (
     <div className="top-margin">
-      <Gallery photos={PARSED_IMAGES} onClick={openLightbox} />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={PARSED_IMAGES.map(x => ({
-                ...x,
-                srcset: x.srcSet,
-                caption: x.title
-              }))}
-            />
-          </Modal>
-        ) : null}
-      </ModalGateway>
+      <Carousel
+        currentIndex={currentImage}
+        views={PARSED_IMAGES.map(x => ({
+          ...x,
+          srcset: x.srcSet,
+          caption: x.title
+        }))}
+      />
     </div>
   );
 }
